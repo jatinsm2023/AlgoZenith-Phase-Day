f471f0a45690458afee1f7f891963550e82a2036
Jatin Mahawar <jatinmahawar08kgpian.iitkgp.ac.in>
2024-08-12 07:13:34 +0530

    Balanced Para

---
+++ Phase 3/Day 37/Balanced Bracket Sequence.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+const int mod = 1e9 + 7;
+
+int dp[1010][1010];
+string s;
+
+int rec(int x, int y)
+{
+    if (y < 0)
+        return 0;
+    if (x == s.length())
+        return (y == 0) ? 1 : 0;
+  
+    if (dp[x][y] != -1)
+        return dp[x][y];
+
+    int ans = 0;
+    if (s[x] == '(')
+        ans = rec(x + 1, y + 1) % mod;
+    else if (s[x] == ')')
+        ans = rec(x + 1, y - 1) % mod;
+    else if (s[x] == '?')
+    {
+        ans = (rec(x + 1, y + 1) + rec(x + 1, y - 1)) % mod;
+    }
+
+    return dp[x][y] = ans;
+}
+
+void solve()
+{
+    cin >> s;
+
+    if (s.length() % 2 != 0)
+    {
+        cout << 0 << '\n';
+    }
+    else
+    {
+        memset(dp, -1, sizeof(dp));
+        cout << rec(0, 0) << '\n';
+    }
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
